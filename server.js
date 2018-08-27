64d01f0ff973ea396d1bb3d9cd3cb98b3beb9a1b
John Tillman <john.w.tillman@gmail.com>
2018-08-26 21:38:37 -0400

    compare friends logic complete

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.get('/api/friends', function(req, res) {
 app.post('/api/friends', function(req, res) {
   const surveyResults = req.body
   console.log(JSON.stringify(surveyResults) + " sent to api!")
+  console.log(findBestMatch(friends, surveyResults))
   res.json(surveyResults)
 })
 
@@ -33,5 +34,23 @@ app.post('/api/friends', function(req, res) {
 app.listen(port, function(req, res) {
   console.log('server is running at ' + port)
 });
+
+function compareScores(user1, user2) {
+  let totalDifference = 0
+  for(let i = 0; i < user1.length; i++) {
+    totalDifference += Math.abs(user1[i] - user2[i]) 
+  }
+  return totalDifference
+}
+
+function findBestMatch(user1, user2) {
+  const matchArr = []
+  for(let i = 0; i < user1.length; i++) {
+    let totalDifference = compareScores(user1[i].scores, user2.scores)
+    matchArr.push(totalDifference)
+  }
+  let lowestDiffIndex = matchArr.indexOf(Math.min(...matchArr))
+  return friends[lowestDiffIndex]
+}
 
 module.exports = app;
