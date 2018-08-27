693293b59bfcf54707c29cbde7c6d335b3571d98
John Tillman <john.w.tillman@gmail.com>
2018-08-26 20:28:58 -0400

    form submit logic completed

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const port = process.env.PORT || 8080;
 
-const friends = require('./data/friends')
-
 //create instance of express object
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+const friends = require('./data/friends')
 
 //server static files
 app.use(express.static(path.join(__dirname, 'public')));
@@ -19,6 +22,11 @@ app.get('/', function(req, res) {
 app.get('/api/friends', function(req, res) {
   res.json(friends);
 });
+
+app.post('/api/friends', function(req, res) {
+  const surveyResults = req.body
+  console.log(surveyResults + " sent to api!")
+})
 
 //start server
 app.listen(port, function(req, res) {
